99bfd02dd7cd047dd590ebdf410eabf9065cba0d
wangjia <jawa0919@163.com>
2020-08-15 12:33:13 +0800

    fix<ocr>：插件的context问题

--- android/src/main/kotlin/top/wiz/wj_ocr/WjOcrPlugin.kt
+++ android/src/main/kotlin/top/wiz/wj_ocr/WjOcrPlugin.kt
@@ -1,11 +1,13 @@
 package top.wiz.wj_ocr
 
-import android.content.Context
+import android.app.Activity
 import android.content.Intent
 import android.util.Log
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -13,15 +15,14 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** WjOcrPlugin */
-class WjOcrPlugin : FlutterPlugin, MethodCallHandler {
+class WjOcrPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
     val TAG = "WjOcrPlugin"
 
-    private lateinit var context: Context
+    private lateinit var activity: Activity
     private lateinit var channel: MethodChannel
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPluginBinding) {
-        context = flutterPluginBinding.applicationContext
         channel = MethodChannel(flutterPluginBinding.flutterEngine.dartExecutor, "wj_ocr")
         channel.setMethodCallHandler(this);
     }
@@ -33,7 +34,7 @@ class WjOcrPlugin : FlutterPlugin, MethodCallHandler {
         fun registerWith(registrar: Registrar) {
             val channel = MethodChannel(registrar.messenger(), "wj_ocr")
             val wjOcrPlugin = WjOcrPlugin()
-            wjOcrPlugin.context = registrar.context()
+            wjOcrPlugin.activity = registrar.activity()
             channel.setMethodCallHandler(wjOcrPlugin)
         }
     }
@@ -43,17 +44,37 @@ class WjOcrPlugin : FlutterPlugin, MethodCallHandler {
             result.success("Android ${android.os.Build.VERSION.RELEASE}")
         } else if (call.method == "startTessOcr") {
             val language: String = call.argument("language") ?: "eng"
-            Log.d(TAG, "onMethodCall: language$language")
+            Log.d(TAG, "onMethodCall: language：$language")
             ocrResult = result;
-            val intent = Intent(context, TessActivity::class.java)
+            val intent = Intent(activity, TessActivity::class.java)
             intent.putExtra("language", language)
-            context.startActivity(intent)
+            activity.startActivity(intent)
         } else {
             result.notImplemented()
         }
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPluginBinding) {
+        Log.d(TAG, "onDetachedFromEngine: ");
         channel.setMethodCallHandler(null)
+    }
+
+    override fun onDetachedFromActivity() {
+        Log.d(TAG, "onDetachedFromActivity: ");
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        Log.d(TAG, "onReattachedToActivityForConfigChanges: ");
+        onAttachedToActivity(binding);
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        Log.d(TAG, "onAttachedToActivity: ");
+        activity = binding.activity;
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        Log.d(TAG, "onDetachedFromActivityForConfigChanges: ");
+        onDetachedFromActivity();
     }
 }
